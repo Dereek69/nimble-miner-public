Fixed the multigpu. Now working correctly with 100% scaling as it uses each gpu on its own

training/core.py
@@ -18,6 +18,7 @@ class Device:
 
     def get_type(self, index):
         # If the index is not provided, check whether a gpu is available
+        #os.environ["CUDA_VISIBLE_DEVICES"] = str(index)
         if index == 0:
             return torch.device(f"cuda:{index}") if torch.cuda.device_count() >= 1 and torch.cuda.is_available() else torch.device("cpu")
         # If the index is provided, check whether the gpu with that index is available. If not, raise an exception that the gpu provided is not available
@@ -54,19 +55,24 @@ class Device:
         # Return the best batch size given the memory size of the device
         # Under 4GB, the batch size is 8
         if self.memory < 4e9:
-            return 8
+            print("Memory is less than 4GB, using batch size 8")
+            return 4
         # Under 8GB, the batch size is 16
         elif self.memory < 8e9:
-            return 16
+            print("Memory is less than 8GB, using batch size 16")
+            return 8
         # Under 16GB, the batch size is 32
         elif self.memory < 16e9:
-            return 32
+            print("Memory is less than 16GB, using batch size 32")
+            return 16
         # Under 32GB, the batch size is 64
         elif self.memory < 32e9:
-            return 64
+            print("Memory is less than 32GB, using batch size 64")
+            return 32
         # Over 32GB, the batch size is 128
         elif self.memory >= 32e9:
-            return 128
+            print("Memory is more than 32GB, using batch size 128")
+            return 64
     
 
 # Class that holds all of the information and methods for a task
@@ -166,8 +172,6 @@ class Task:
 
         device = self.device.info
         model.to(device)
-        print(f"Device: {device}")
-        print(f"Model: {model.device}")
 
         dataset = load_dataset(self.task["dataset_name"])
         tokenized_datasets = dataset.map(tokenize_function, batched=True)

training/utils.py
@@ -104,9 +104,6 @@ def identify_devices(addr_list, devices_index):
     
     #get all the available devices and their properties
     devices = [Device(i) for i in range(torch.cuda.device_count())]
-    print("\nAvailable devices: ")
-    for i in range(len(devices)):
-        print(f"Device {i}: {devices[i].name}")
     
     #If there are no gpus, use the cpu
     if len(devices) == 0:
@@ -122,6 +119,6 @@ def identify_devices(addr_list, devices_index):
     
     #Replace the device index with the device object in the devices list
     for i in range(len(addr_list)):
-        addr_list[i] = (addr_list[i][0], devices[addr_list[i][1]])
+        addr_list[i] = (addr_list[i][0], devices[0])  #IT SHOULD HAVE BEEN devices[devices_index[i]] INSTEAD OF devices[0] IF RUN ON DDP
 
     return addr_list